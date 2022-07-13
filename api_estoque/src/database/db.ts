a08b84f0b83fa79cabacedb8cecdc77ab3a2830d
Arthur Xavier <arthurcsxavier@gmail.com>
2022-07-13 14:35:54 -0300

    add query repository e database connection

--- api_estoque/src/database/db.ts
+++ api_estoque/src/database/db.ts
@@ -1,6 +1,6 @@
 import {Pool} from "pg";
 
-const connectionString = ``;
+const connectionString = `postgres://wgedgsxc:f7pqCYQTHTMP96NLzhadHJgKe0npHegH@motty.db.elephantsql.com/wgedgsxc`;
 
 const db = new Pool({connectionString});
 
--- api_estoque/src/repositories/warehouseRepository.ts
+++ api_estoque/src/repositories/warehouseRepository.ts
@@ -1,6 +1,73 @@
 import db from "../database/db";
+import Inventory from "../model/inventoryModel";
 
 class warehouseRepository {
 
+    async getAllItems(): Promise<Inventory[]> {
+        const query = `
+        SELECT * FROM warehouse
+        `;
+
+        const {rows} = await db.query<Inventory>(query);
+        return rows || [];
+    }
+
+    async getItemByName(name: string): Promise<any> {
+        const query = `
+        SELECT product_name, quantity, price, image,
+        FROM warehouse
+        WHERE product_name ILIKE '${name}%'`;
+
+        const {rows} = await db.query<Inventory>(query);
+
+        return rows || []
+    }
+
+    async postItem(item: Inventory): Promise<void> {
+        const query = `
+        INSERT INTO warehouse (
+            product_name,
+            quantity,
+            type,
+            price,
+            image
+        )
+        VALUES ($1, $2, $3, $4, $5)
+        `;
+
+        const values = [item.product_name, item.quantity, item.type, item.price, item.image];
+
+        await db.query(query, values);
+    }
+
+    async updateItem(uuid: string): Promise<void> {
+        const query = `
+        UPDATE warehouse
+        SET
+            product_name = $1,
+            quantity = $2,
+            type = $3,
+            price = $4,
+            image = $5
+        WHERE uuid = $6
+        `;
+
+        const values = [uuid];
+
+        await db.query(query, values);
+    }
+
+    async deleteItem(uuid: string): Promise<void> {
+        const query = `
+        DELETE FROM warehouse
+        WHERE uuid = $1
+        `;
+
+        const values = [uuid];
+
+        await db.query(query);
+    }
 
 }
+
+export default new warehouseRepository();
